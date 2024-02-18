d5fb4de3e126ab03fa34b56e75cbd3ba4933180c
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2024-02-19 01:24:04 +0400

    Temporary commit

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,13 +1,20 @@
 export default {
   index: {
-    title: "Home",
-    type: "page",
-    display: "hidden",
+    title: 'Home',
+    type: 'page',
+    display: 'hidden',
     theme: {
-      layout: "raw"
+      layout: 'raw'
     }
   },
   'linux-programming': {
+    title: 'Linux Programming',
+    type: 'page',
+    display: 'hidden'
+  },
+  'operating-systems': {
+    title: 'Operating Systems',
+    type: 'page',
     display: 'hidden'
   },
   courses: {
--- pages/linux-programming/index.jsx
+++
@@ -1,5 +0,0 @@
-import AutoNavigate from "components/common/auto-navigate"
-
-export default function LinuxProgrammingIndex(){
-    return <AutoNavigate to="/linux-programming/introduction" replace={true} />
-}
