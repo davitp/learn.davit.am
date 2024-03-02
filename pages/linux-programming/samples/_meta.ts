06fa7b12165a4aede12606ab054ba1f661783b1c
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2024-03-02 22:27:41 +0400

    Added samples

---
+++ pages/linux-programming/samples/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  'makefile': 'Using Makefile',
+  'open-file': 'Opening a file',
+  'read-file': 'Reading a file',
+  'copy-file': 'Copy a file',
+  'redirect-output': 'Redirect the output',
+  'make-hole': 'Creating holes',
+  'detect-holes': 'Detect holes',
+  'create-process': 'Create a process',
+  'create-thread': 'Create a thread'
+}
