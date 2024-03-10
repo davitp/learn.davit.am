6982f7f1bfdc3cb9d6550922bcbbe1c9d02f023f
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2024-03-10 15:01:38 +0400

    Added introduction to linux programming

--- pages/linux-programming/topics/_meta.ts
+++ pages/linux-programming/topics/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   'hardware-and-software': 'Hardware and Software',
   'file-systems-overview': 'File Systems Overview',
+  'linux-programming': 'Linux Programming',
   'system-calls': 'System Calls'
 }
